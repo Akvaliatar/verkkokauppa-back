d8c63ebf781e1c84a6c06839d665f45856104ab3
Valtpit <77121949+Valtpit@users.noreply.github.com>
2021-11-17 19:12:51 +0200

    Create insertInto.sql
    tiedosto databasen täydentämiseen

---
+++ insertInto.sql
@@ -0,0 +1,12 @@
+/* Tästä tiedostosta on helppo lisätä databaseen tuotteita yms */
+
+/*tuoteryhmien numerot
+1 = Eläinherkut
+2 = Eläintenhoitotuotteet
+3 = Askartelutarvikkeet
+4 = Asusteet
+5 = Palvelut
+6 = Liput
+*/
+
+-- INSERT INTO tuote VALUES (tuotenro,'tuotenimi', hinta, kustannus, trnro, väri, määrä, koko, LankaTyyppiEläin, Pituus);
