b8941993d6ee48dd49a48b854edd936d1c8f43a0
Valtpit <77121949+Valtpit@users.noreply.github.com>
2021-11-17 18:11:14 +0200

    Update ehdotus.sql

--- ehdotus.sql
+++ ehdotus.sql
@@ -5,7 +5,7 @@ trnimi CHAR(10),
 CONSTRAINT tuoteryhma_pk PRIMARY KEY (trnro)
 ) ;
 
---values
+--values tuoteryhmä
 INSERT INTO tuoteryhma VALUES (1,'Eläinherkut');
 INSERT INTO tuoteryhma VALUES (2,'Eläintenhoitotuotteet');
 INSERT INTO tuoteryhma VALUES (3,'Askartelutarvikkeet');
@@ -31,7 +31,48 @@ CONSTRAINT tuote_ryhma_fk FOREIGN KEY (trnro)
            REFERENCES tuoteryhma (trnro)
 ) ;
 
---values
+--values tuote
 --INSERT INTO tuote VALUES (tuotenro,'tuotenimi', hinta, kustannus, trnro, väri, määrä, koko, LankaTyyppiEläin, Pituus);
 INSERT INTO tuote VALUES (1,'lihapulla', 2.50, 1.50, 1, NULL, 5, NULL, NULL, NULL);
 
+-- asiakas
+CREATE TABLE asiakas (
+asID CHAR(6),
+asETUnimi CHAR(20) NOT NULL,
+asSUKUnimi CHAR(20) NOT NULL,
+postinro CHAR(5), 
+postitmp CHAR(10),
+puh INTEGER, 
+sposti CHAR(64), 
+CONSTRAINT asiakas_pk PRIMARY KEY (asID),
+CONSTRAINT asnimi_un UNIQUE (asnimi)
+) ;
+
+
+--tilaus
+CREATE TABLE tilaus (
+tilausID INTEGER NOT NULL,
+asID CHAR(6) NOT NULL, 
+tilauspvm DATETIME NOT NULL, /* tämä muutettava käytettävän sql-tuotteen mukaan*/
+tapa CHAR(1) NOT NULL,
+tila CHAR(1),
+CONSTRAINT tilaus_pk PRIMARY KEY (tilausnro),
+CONSTRAINT tilaus_asiakas_fk FOREIGN KEY (astunnus) 
+           REFERENCES asiakas (astunnus)
+) ;
+
+--values tilaus
+
+
+-- Tilausrivi-taulu
+CREATE TABLE tilausrivi (
+tilausID INTEGER NOT NULL,
+rivinro SMALLINT NOT NULL,
+tuotenro INTEGER, 
+kpl INTEGER,
+CONSTRAINT tilausrivi_pk PRIMARY KEY (tilausnro, rivinro),
+CONSTRAINT tilausrivi_tuote_fk FOREIGN KEY (tuotenro) 
+           REFERENCES tuote (tuotenro)
+);
+
+--values tilausrivi
